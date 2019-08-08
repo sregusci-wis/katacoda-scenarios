ec9c1072b367e2d53941f8c10fd327af8e97a572
Santiago Regusci <sregusci@gicla.com>
2019-08-08 14:43:07 -0300

    agrego intro

--- uilayouts/crear-proyecto/assets/step1-foreground.sh
+++ uilayouts/crear-proyecto/assets/step1-foreground.sh
@@ -2,5 +2,5 @@
 
 stty -echo
 clear
-sleep 3 && /usr/games/cowsay -f daemon BIENVENIDO A EL ENTRENAMIENTO DE AKKA.NET
+sleep 3 && /usr/games/cowsay BIENVENIDO A EL ENTRENAMIENTO DE AKKA.NET
 stty echo
